af785eeca0bbc0a378319eae5058123b7ad020bd
Kevin Zhuang <K-Zhuang@users.noreply.github.com>
2024-04-01 23:43:26 +0000

    Card dealing logic and simple UI

--- nihuawocai/src/cards/Game2.tsx
+++ nihuawocai/src/cards/Game2.tsx
@@ -1,17 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../App.css';
+import { Deck, Hand } from './Game2Entities';
 
 type Game2Props = {
   toMainPage: () => void,
 }
 
 function Game2(props: Game2Props) {
-  document.title = "升级";
+  useEffect(() => { document.title = "升级" }, []);
+
+  // state used for triggering rerenders
+  const [hack, setHack] = useState(0);
+
+  const [deck, _setDeck] = useState(new Deck(2));
+
+  const [hands, _setHands] = useState([new Hand(), new Hand(), new Hand(), new Hand()]);
 
   return (
     <div className="App">
       <div>{"<Game here>"}</div>
       <button onClick={props.toMainPage}>Back to main page</button>
+      <button 
+        onClick={() => {
+          const dealPhase = setInterval(
+            () => {
+              if (deck.isDeckEmpty()) {
+                clearInterval(dealPhase);
+              }
+              hands.forEach(hand => {
+                const card = deck.dealCard();
+                card != null && hand.addCardToHand(card);
+              });
+              setHack(hack+1);
+            }, 
+            1000,
+          ); 
+        }}>
+        Deal!
+      </button>
+      <button
+        onClick={() => {
+          hands.forEach(hand => {
+            const card = deck.dealCard();
+            card != null && hand.addCardToHand(card);
+          });
+          setHack(hack+1);
+        }}>
+        Deal one round!
+      </button>
+      <div>{deck.cards.map(c => c.asString()).join(', ')}</div>
+      {hands.map(hand => {
+        return <div>Hand: {Array.from(hand.cards.values()).map(l => l.map(c => c.asString())).join(', ')}</div>;
+      })}
     </div>
   );
 }
--- nihuawocai/src/cards/Game2Entities.ts
+++ nihuawocai/src/cards/Game2Entities.ts
@@ -1,4 +1,4 @@
-enum Suit {
+export enum Suit {
     JOKER,
     SPADE,
     HEART,
@@ -6,7 +6,8 @@ enum Suit {
     DIAMOND,
 }
 
-type Value = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13;
+export const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] as const;
+export type Value = typeof values[number];
 
 export class Card {
     suit: Suit;
@@ -16,43 +17,98 @@ export class Card {
         this.suit = suit;
         this.value = value;
     }
+
+    asString() {
+        switch (this.suit) {
+            case Suit.JOKER:
+                if (this.value == 1) {
+                    return "小王";
+                }
+                return "大王";
+            case Suit.SPADE:
+                return this.value+"S";
+            case Suit.HEART:
+                return this.value+"H";
+            case Suit.CLUB:
+                return this.value+"C";
+            case Suit.DIAMOND:
+                return this.value+"D";
+        }
+    }
 }
 
 export class Deck {
-    cards: Array<Card>;
+    cards: Card[];
     constructor(numDecks: number) {
-        this.cards = [new Card(Suit.SPADE, 1)]
+        this.cards = [];
+        for (let i = 0; i < numDecks; i++) {
+            this.cards.push(...Deck.oneDeckOfCards());
+        }
+
+        // Shuffle using Durstenfeld Shuffle
+        for (var i = this.cards.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            const temp = this.cards[i];
+            this.cards[i] = this.cards[j];
+            this.cards[j] = temp;
+        }
+    }
+
+    isDeckEmpty() {
+        return this.cards.length === 0;
+    }
+
+    dealCard() {
+        return this.cards.pop();
+    }
+
+    static oneDeckOfCards(): Card[] {
+        var cards = [];
+        // Numbered cards
+        values.forEach(value => {
+            cards.push(
+                new Card(Suit.SPADE, value), 
+                new Card(Suit.HEART, value), 
+                new Card(Suit.CLUB, value), 
+                new Card(Suit.DIAMOND, value)
+            );
+        });
+
+        // Jokers
+        cards.push(new Card(Suit.JOKER, 1));
+        cards.push(new Card(Suit.JOKER, 2));
+        return cards;
     }
 }
 
 export class Hand {
-    cards: $ReadOnlyMap<Suit, Value>;
+    cards: Map<Suit, Card[]>;
 
     constructor() {
-        this.cards;
+        this.cards = new Map();
+    }
+
+    addCardToHand(card: Card) {
+        let s = this.cards.get(card.suit) ?? [];
+        s.push(card);
+        this.cards.set(card.suit, s.sort((c1, c2) => c1.value - c2.value));
     }
 }
 
-export class Player {
-    points: number = 0;
-    hand: Hand = [];
+// export class Player {
+//     points: number = 0;
+//     hand: Hand = [];
+// }
+
+export class DrawPhaseEnvironment {
+    
 }
 
-export class Team {
-
-}
 
 export class PlayedHand {
 
 }
 
-export class GameEnvironment {
-    trump: Suit;
-    deck: ReadonlyArray<Card>;
+export class PlayPhaseEnvironment {
 
-    constructor(trump: Suit) {
-        this.trump = trump;
-        // 
-        this.deck = ;
-    }
 }
